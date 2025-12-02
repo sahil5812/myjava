a3e81e13630177e1b1c492ef469c77ccd8b24d54
sahil5812 <khanabusahil506@gmail.com>
2025-12-02 16:30:53 +0530

    Updated hangman

--- src/Hangman_game.java
+++ src/Hangman_game.java
@@ -1,58 +1,90 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Hangman_game {
-    public static void main(String[] args){
-        String word = "pizza";
-        Scanner sc = new Scanner(System.in);
-        ArrayList<Character>wordState = new ArrayList<>();
-        int wrong_guess = 0;
+    public static void main(String[] args) {
 
-        for (int i = 0; i<word.length();i++){
+        // JAVA HANGMAN GAME
+
+        String filePath = "words.txt";
+        ArrayList<String> words = new ArrayList<>();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(filePath))){
+            String line;
+            while((line = reader.readLine()) != null){
+                words.add(line.trim());
+            }
+        }
+        catch(IOException e){
+            System.out.println("Something went wrong");
+        }
+
+        Random random = new Random();
+
+        String word = words.get(random.nextInt(words.size()));
+
+        Scanner scanner = new Scanner(System.in);
+        ArrayList<Character> wordState = new ArrayList<>();
+        int wrongGuesses = 0;
+
+        for(int i = 0; i < word.length(); i++){
             wordState.add('_');
         }
+
         System.out.println("************************");
         System.out.println("Welcome to Java Hangman!");
         System.out.println("************************");
 
-        while(wrong_guess< 6){
+        while(wrongGuesses < 6){
 
-            System.out.println(getHangmanArt(wrong_guess));
+            System.out.print(getHangmanArt(wrongGuesses));
 
             System.out.print("Word: ");
-            for (char c : wordState){
-                System.out.print(c+" ");
+
+            for(char c : wordState){
+                System.out.print(c + " ");
             }
             System.out.println();
-            System.out.print("Guess a letter: ");
-            char guess =sc.next().toLowerCase().charAt(0);
 
-            if (word.indexOf(guess) >= 0){
+            System.out.print("Guess a letter: ");
+            char guess = scanner.next().toLowerCase().charAt(0);
+
+            if(word.indexOf(guess) >= 0){
                 System.out.println("Correct guess!");
-                for (int i = 0; i<word.length();i++){
-                    if (word.charAt(i) == guess){
-                        wordState.set(i,guess);
+
+                for(int i = 0; i < word.length(); i++){
+                    if(word.charAt(i) == guess){
+                        wordState.set(i, guess);
                     }
                 }
-                if (!wordState.contains('_')){
-                    System.out.println(getHangmanArt(wrong_guess));
-                    System.out.println("You win!");
-                    System.out.println("The word was: "+word);
+
+                if(!wordState.contains('_')){
+                    System.out.print(getHangmanArt(wrongGuesses));
+                    System.out.println("YOU WIN!");
+                    System.out.println("The word was: " + word);
                     break;
                 }
-            }else {
-                wrong_guess++;
+            }
+            else{
+                wrongGuesses++;
                 System.out.println("Wrong guess!");
             }
         }
-        if (wrong_guess > 6){
-            System.out.println(getHangmanArt(wrong_guess));
-            System.out.println("Game over!");
-            System.out.println("The word was: "+ word);
+
+        if(wrongGuesses >= 6){
+            System.out.print(getHangmanArt(wrongGuesses));
+            System.out.println("GAME OVER!");
+            System.out.println("The word was: " + word);
         }
 
-    }
+        scanner.close();
 
+    }
     static String getHangmanArt(int wrongGuesses){
 
         return switch(wrongGuesses){
